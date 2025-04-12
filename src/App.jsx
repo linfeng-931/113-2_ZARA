6df8caedffc5242d11093d1727b68a70e69a7abf
linfeng-931 <s111219030@stu.ntue.edu.tw>
2025-04-12 11:16:09 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Route path = "/" element = {<Home/>}/>
+
+        <Route path = "products">
+          <Route path = "category/:categoryName" element={<Category/>}/>
+          <Route path = "id/:productId" element = {<Product/>}/>
+          <Route path = "shopping-cart" element = {<ShoppingCart/>}/>
+        </Route>
+
+        {/*以下為有閒情再做的東西
+        <Route path = "user">
+          login、signIn部份看是要在其他頁面做彈跳視窗還是拉出來獨立頁面
+          <Route path = "login" element = {<Login/>}/>
+          <Route path = "sign-in" element = {<SignIn/>}/>
+          <Route path = "user-detail" element = {<UserDetail/>}/>
+        </Route>*/}
+        
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
