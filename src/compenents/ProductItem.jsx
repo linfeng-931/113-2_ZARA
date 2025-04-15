fdc8d06129b7dcb74728d71bec3f6efdde2d8721
jen067 <=>
2025-04-15 22:45:47 +0800

    修正 json 檔與新增商品類別清單

--- src/compenents/ProductItem.jsx
+++ src/compenents/ProductItem.jsx
@@ -1,17 +1,37 @@
 import { Link } from "react-router";
+import { useState } from "react";
 
 const ProductItem = ({ product }) => {
-  const headImg = product.class[0].img[0];
-  console.log(headImg);
+  const [headImg, setHeadImg] = useState(product.class[0].cover);
+  const handleColorClick = (index) => {
+    setHeadImg(product.class[index].cover);
+  };
+
+  const colors = product.class.map((x, index) => {
+    return (
+      <div
+        key={index}
+        onClick={() => handleColorClick(index)}
+        style={{ backgroundColor: x.color_num }}
+        className="w-[16px] h-[16px] rounded-full border cursor-pointer mx-1"
+        title={x.color}
+      ></div>
+    );
+  });
 
   return (
     <div className="col-span-4 grid grid-cols-4">
-      <section className="col-span-3 mb-8">
+      <section className="col-span-3 mb-8 text-sm flex flex-col gap-2 text-left ">
         <img
           className="w-full max-h-120 object-cover object-center"
           src={headImg}
           alt=""
         />
+        <p className="name">{product.name}</p>
+        <p className="price font-bold">
+          $ <span className="ml-1">{product.price}</span>
+        </p>
+        <div className="colors flex translate-x-[-6px]">{colors}</div>
       </section>
       <div className="col-span-1"></div>
     </div>
