a8760619e0aea77baa9797ce3bf95b5372139e63
linfeng-931 <s111219030@stu.ntue.edu.tw>
2025-04-15 16:24:37 +0800

    新增hamber、color mode響應式、商品尺寸表

--- src/compenents/Header.jsx
+++ src/compenents/Header.jsx
@@ -1,35 +1,35 @@
 import { ShoppingCart, CircleUserRound, CircleHelp, Moon, Sun} from "lucide-react";
-import HamMenu from "./HamMenu";
+import Menu from "./Menu";
 import SetColorMode from "./SetColorMode";
 
 function Header(){
     return(
         <>
             <div className="header-container flex w-screen h-28 items-center justify-between z-100">
-                <div className="left flex gap-17 pl-15 items-center">
-                    <HamMenu/>
-                    <img className="h-11 invisible md:visible dark:hidden" src="/public/image/logo2-1.png"></img>
-                    <img className="h-11 invisible md:visible light:hidden" src="/public/image/logo2-2.PNG"></img>
+                <div className="left flex gap-20 pl-7 md:pl-15 items-center">
+                    <Menu/>
+                    <img className="h-0 md:h-11 dark:hidden z-9999" src="/public/image/logo2-1.png"></img>
+                    <img className="h-0 md:h-11 light:hidden z-9999" src="/public/image/logo2-2.PNG"></img>
                 </div>
-                <div className="right flex pr-15 gap-8 items-center">
+                <div className="right flex pr-7 gap-5 md:pr-15 md:gap-8 items-center">
                     <SetColorMode/>
 
-                    <div className="line h-12 w-[1px] bg-black dark:bg-white opacity-50"></div> 
+                    <div className="line hidden md:block h-12 w-[1px] bg-black dark:bg-white opacity-50"></div> 
 
                     <div className="member flex flex-col items-center gap-1.5 whitespace-nowrap
                     hover:opacity-50 cursor-pointer">
                         <CircleUserRound />
-                        <p>登入</p>
+                        <p>Login</p>
                     </div>
                     <div className="member flex flex-col items-center gap-1.5 whitespace-nowrap
                     hover:opacity-50 cursor-pointer">
                         <ShoppingCart />
-                        <p>購物籃</p>
+                        <p>Cart</p>
                     </div>
                     <div className="member flex flex-col items-center gap-1.5 whitespace-nowrap
                     hover:opacity-50 cursor-pointer">
                         <CircleHelp />
-                        <p>協助</p>
+                        <p>Help</p>
                     </div>
                 </div>
             </div>
